1efd646420b1fb522f0182196fca917d8219d467
ParmarManthanrajsinh <manthanrajsinh@gmail.com>
2025-07-31 23:28:19 +0530

    Refactor SQL imports in ProductServlet.java

--- WBJP/manual_solution/P2/src/main/java/ProductServlet.java
+++ WBJP/manual_solution/P2/src/main/java/ProductServlet.java
@@ -1,10 +1,6 @@
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.Statement;
+import java.sql.*;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
